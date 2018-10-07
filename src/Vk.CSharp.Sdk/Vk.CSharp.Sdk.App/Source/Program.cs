cb38542a66005f114c1699c9bb663f58249ae6fd
Victor <43318916+pDopnex@users.noreply.github.com>
2018-10-07 12:55:20 +0300

    Создал решение и основные проекты

---
+++ src/Vk.CSharp.Sdk/Vk.CSharp.Sdk.App/Source/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Vk.CSharp.Sdk.App.Source
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+            Console.WriteLine("vk-c-sharp-sdk");
+            Console.ReadKey();
+        }
+    }
+}
